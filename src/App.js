177cb37329ebe21f43773c5b0d3950b4c1f30bcd
Ngo Tien Dat <ngotiendat345@icloud.com>
2021-01-22 00:45:46 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Loading from './Loading';
+import Tour from './Tour';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url = 'https://course-api.com/react-tours-project';
+
+function App(){
+    const [tours,setTours]= useState([]);
+    const [loading,setLoading] = useState(true);
+    async function fetchData(){
+
+        const response = await fetch(url);
+        const data = await response.json();
+        setTours(data);
+        console.log(data);
+        setLoading(false);
+    }
+    function ClearTour(id){
+        const newTour = tours.filter((tour)=>tour.id!==id);
+        setTours(newTour);
+    }
+    useEffect(()=>{
+        fetchData();
+    },[])
+    if(loading){
+        return <Loading />
+    }
+    return (
+        <main>
+            <section>
+                <div className="title"><h2>our tours</h2><div className="underline"></div></div>
+                <div>
+                {tours.map((tour)=>{
+                    return <Tour {...tour} ClearTour={ClearTour} key={tour.id}/>
+                })}
+                </div>
+            </section>
+        </main>
+    )
 }
 
 export default App;
